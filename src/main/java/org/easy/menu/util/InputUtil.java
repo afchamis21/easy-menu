697eee3aa46d444014cd186e47549077ec38e61d
André <andrechamis21@gmail.com>
2025-01-02 14:44:49 -0300

    feat: Implemented InputUtil.getLongInput

--- src/main/java/org/easy/menu/util/InputUtil.java
+++ src/main/java/org/easy/menu/util/InputUtil.java
@@ -1,7 +1,19 @@
 package org.easy.menu.util;
 
+import java.util.Scanner;
+
 public class InputUtil {
+    private static final Scanner scanner = new Scanner(System.in);
     public static long getLongInput(String s) {
-        return 0L; // TODO
+        try {
+            System.out.print(s);
+            long valor = scanner.nextLong();
+            scanner.nextLine();
+            return valor;
+        } catch (Exception e) {
+            System.out.println("Enter a valid long number!");
+            scanner.nextLine();
+            return getLongInput(s);
+        }
     }
 }
